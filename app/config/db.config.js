78ccefc3f2c6b021de6820943eefa7c03b419c9d
ArpitMehta <arpitmehta89@gmail.com>
2024-10-24 23:24:52 +0530

    Add limits-service.properties

---
+++ app/config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url: "mongodb://192.168.29.220:27017/Athena_db"
+};
---
+++ app/routes/turorial.routes.js
@@ -0,0 +1,31 @@
+module.exports = app => {
+  const tutorials = require("../controllers/tutorial.controller.js");
+
+  var router = require("express").Router();
+
+  // Create a new Tutorial
+  router.post("/", tutorials.create);
+
+  // Retrieve all Tutorials
+  router.get("/", tutorials.findAll);
+
+  // Retrieve all published Tutorials
+  router.get("/app/allData", tutorials.findAllPublished);
+
+  // Retrieve a single Tutorial with id
+  router.get("/getdata", tutorials.findOne);
+
+  // Update a Tutorial with id
+  router.put("/:id", tutorials.update);
+
+  // Delete a Tutorial with id
+  router.delete("/:id", tutorials.delete);
+
+  // Create a new Tutorial
+  router.delete("/", tutorials.deleteAll);
+
+  app.use("/api/tutorials", router);
+  app.use("/app/allData", router);
+  app.use("/getdata", router);
+
+};
